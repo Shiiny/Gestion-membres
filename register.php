b179b8e8968290e25882be6be11ac02d60edec1c
Shiny <oury.anthony@neuf.fr>
2018-01-24 12:37:05 +0100

    Création du projet et des fichiers

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php require 'inc/header.php'; ?>
+
+<h1>S'inscrire</h1>
+
+
+<form action="" method="POST">
+	<div class="form-group">
+		<label for="">Pseudo</label>
+		<input type="text" name="username" class="form-control" required />
+	</div>
+
+	<div class="form-group">
+		<label for="">Email</label>
+		<input type="text" name="email" class="form-control" required />
+	</div>
+
+	<div class="form-group">
+		<label for="">Mot de passe</label>
+		<input type="password" name="password" class="form-control" required />
+	</div>
+
+	<div class="form-group">
+		<label for="">Confirmez votre mot de passe</label>
+		<input type="password" name="password_confirm" class="form-control" required />
+	</div>
+
+	<button type="submit" class="btn btn-primary">M'inscrire</button>
+</form>
+
+<?php var_dump($_POST); ?>
+
+<?php require 'inc/footer.php'; ?>
