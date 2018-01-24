bcc0d3c01dfee1ba8c6f10b0884ecd21bde46635
Anthony Oury <oury.anthony@neuf.fr>
2018-01-24 23:24:35 +0100

    Ajout de function.php

--- register.php
+++ register.php
@@ -4,26 +4,46 @@
 
 if(!empty($_POST)) {
 	$errors = [];
+	require_once 'inc/db.php';
 
 	if(empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username'])) {
 		$errors['username'] = "Votre pseudo n'est pas valide !";
 	}
+	else {
+		$req = $pdo->prepare('SELECT id FROM users WHERE username = ?');
+		$req->execute([$_POST['username']]);
+		$user = $req->fetch();
+		if($user) {
+			$errors['username'] = "Ce pseudo est déjà utilisé";
+		}
+	}
 	if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 		$errors['email'] = "Votre email n'est pas valide !";
 	}
-	if(empty($_POST['password']) || $_POST['password'] != $_POST['password_confirm']) {
+	else {
+		$req = $pdo->prepare('SELECT id FROM users WHERE email = ?');
+		$req->execute([$_POST['email']]);
+		$user = $req->fetch();
+		if($user) {
+			$errors['email'] = "Cet adresse e-mail est déjà utilisé";
+		}
+	}
+	if(empty($_POST['password']) || $_POST['password'] !== $_POST['password_confirm']) {
 		$errors['password'] = "Vous devez rentrer un mot de passe valide";
 	}
 
 
-if(empty($errors)) {
-	require_once 'inc/db.php';
-	$req = $pdo->prepare("INSERT INTO users SET username = ?, password = ?, email = ?");
-	$req->execute([$_POST['username'], sha1($_POST['password']), $_POST['email']])
-}
-
-	var_dump($errors);
-
+	if(empty($errors)) {
+		$req = $pdo->prepare("INSERT INTO users SET username = ?, password = ?, email = ?, confirmation_token = ?");
+		$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+		$token = str_random(60);
+		$req->execute([$_POST['username'], $password, $_POST['email'], $token]);
+		$user_id = $pdo->lastInsertId();
+		$mail_msg = "Afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://localhost/Gestion-membres/confirm.php?id=$user_id&token=$token";
+		mail($_POST['email'], "Confirmation de votre compte", $mail_msg);
+		//header('Location: login.php');
+		die(var_dump(imap_mail($_POST['email'], "Confirmation de votre compte", $mail_msg)));
+	}
 }
 
 ?>
@@ -31,6 +51,17 @@ if(empty($errors)) {
 
 <h1>S'inscrire</h1>
 
+<?php if(!empty($errors)): ?>
+<div class="alert alert-danger">
+	<p>Vous n'avez pas rempli le formulaire correctement</p>
+	<ul>
+	<?php foreach ($errors as $error): ?>
+		<li><?= $error; ?></li>
+	<?php endforeach; ?>
+	</ul>
+
+</div>
+<?php endif; ?>
 
 <form action="" method="POST">
 	<div class="form-group">
